13043ed1a7d127f8dd62fdeeed497df70a00fc73
MrFlyingbananas <mrflyingbananas@yahoo.com>
2016-07-19 13:58:20 -0500

    Added a get tests by subject method

--- src/QuestionPane/DBReader.java
+++ src/QuestionPane/DBReader.java
@@ -21,7 +21,11 @@ public class DBReader {
     private enum SelectionID {
         All("*"),
         SubjectLong("SubjectLong"),
-        SubjectID("SubjectID");
+        SubjectID("SubjectID"),
+        TestSetID("TestSetID"),
+        TestID("TestID"),
+        TestName("TestName"),
+        Subtitle("Subtitle");
         final String id;
         SelectionID(String id){
             this.id = id;
@@ -79,15 +83,15 @@ public class DBReader {
         }
         random = new Random();
     }
-    private ResultSet getData(TableID table, SelectionID data, String filter){
+    private ResultSet getData(SelectionID selection, TableID table, String filter){
         try {
-            return stmnt.executeQuery("SELECT " + data.getSelectionID() + " FROM " + table.getTableID() + " " + filter);
+            return stmnt.executeQuery("SELECT " + selection.getSelectionID() + " FROM " + table.getTableID() + " " + filter);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
-    private ResultSet getData(TableID table, SelectionID[] selections, String filter){
+    private ResultSet getData(SelectionID[] selections, TableID table, String filter){
         try {
             String selection = "";
             for(int i = 0; i < selections.length; i++){
@@ -101,9 +105,26 @@ public class DBReader {
             e.printStackTrace();
         }
         return null;
+
+    }
+    private ResultSet getData(SelectionID selection, TableID[] tables, String filter){
+        try {
+            String table = "";
+            for(int i = 0; i < tables.length; i++){
+                table += tables[i].getTableID();
+                if(i + 1 != tables.length){
+                    table += " AND ";
+                }
+            }
+            return stmnt.executeQuery("SELECT " + selection.getSelectionID() + " FROM " + table + " " + filter);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
     public List<Question> getQuestionListBySubject(Subject subject, ListOrder order){
-        ResultSet rs = getData(TableID.Questions, SelectionID.All, "WHERE SubjectID=" + subject.subjectID);
+        ResultSet rs = getData(SelectionID.All, TableID.Questions, "WHERE SubjectID=" + subject.subjectID);
         List<Question> questions = new ArrayList<>();
         try {
             while(rs.next()){
@@ -124,10 +145,48 @@ public class DBReader {
 
     public String getSubjectString(Subject subject){
         try {
-            return getData(TableID.Subjets, SelectionID.SubjectLong, "WHERE " + SelectionID.SubjectID.getSelectionID() + "=" + subject.getSubjectID()).getString(SelectionID.SubjectLong.getSelectionID());
+            return getData(SelectionID.SubjectLong, TableID.Subjets, "WHERE " + SelectionID.SubjectID.getSelectionID() + "=" + subject.getSubjectID()).getString(SelectionID.SubjectLong.getSelectionID());
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public List<Test> getTestListBySubject(Subject subject, ListOrder questionOrder){
+        List<Test> tests = new ArrayList<Test>();
+        int testSetID = 0;
+        try {
+            testSetID = getData(SelectionID.TestSetID, TableID.TestSets, "WHERE " + SelectionID.SubjectID.getSelectionID() + "=" + subject.getSubjectID()).getInt(SelectionID.TestSetID.getSelectionID());
+            ResultSet testSets = getData(SelectionID.All, TableID.Tests, "WHERE " + SelectionID.TestSetID.getSelectionID() + "=" + testSetID);
+            List<String> subtitles = new ArrayList<String>();
+            List<Integer> testIDs = new ArrayList<Integer>(), testNames = new ArrayList<Integer>();
+            List<List<Question>> questionSets = new ArrayList<>();
+            while (testSets.next()) {
+                testIDs.add(testSets.getInt(SelectionID.TestID.getSelectionID()));
+                testNames.add(testSets.getInt(SelectionID.TestName.getSelectionID()));
+                subtitles.add(testSets.getString(SelectionID.Subtitle.getSelectionID()));
+            }
+            for(int testID : testIDs){
+                ResultSet resultSet = getData(SelectionID.All, TableID.Questions, "WHERE " + SelectionID.TestID.getSelectionID() + "=" + testID);
+                List<Question> questions = new ArrayList<Question>();
+                while(resultSet.next()){
+                    questions.add(new Question(resultSet));
+                }
+                switch(questionOrder){
+                    case Random:
+                        Collections.shuffle(questions);
+                        break;
+                    case Inverse:
+                        Collections.reverse(questions);
+                }
+                questionSets.add(questions);
+            }
+            for(int i = 0; i < questionSets.size(); i++){
+                tests.add(new Test(subject, testIDs.remove(0), testNames.remove(0), subtitles.remove(0), questionSets.remove(0)));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tests;
     }
 }
