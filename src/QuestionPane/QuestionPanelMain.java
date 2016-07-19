53db05af976c3b8dacc40a1ab1bb1b8285fa2bd2
MrFlyingbananas <mrflyingbananas@yahoo.com>
2016-07-18 23:13:45 -0500

    Added look and feel change

--- src/QuestionPane/QuestionPanelMain.java
+++ src/QuestionPane/QuestionPanelMain.java
@@ -1,16 +1,27 @@
 package QuestionPane;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import java.awt.Dimension;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 /**
  * Created by 120658 on 3/15/2016.
  */
 public class QuestionPanelMain {
     public QuestionPanelMain(){
+        try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            System.out.println(Arrays.toString(UIManager.getInstalledLookAndFeels()));
+        }
+        catch (UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         JFrame frame = new JFrame();
         frame.setPreferredSize(new Dimension(600, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
