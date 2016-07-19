35b3ff6746d5d10d6469f4bd7430f2cb2adcbf53
MrFlyingbananas <mrflyingbananas@yahoo.com>
2016-07-18 23:12:58 -0500

    Added Helper Methods Create question return order param Create get subject name Created selectionID enum to make requests easier

--- src/QuestionPane/DBReader.java
+++ src/QuestionPane/DBReader.java
@@ -5,12 +5,31 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by cisom on 6/23/16.
  */
 public class DBReader {
+    public enum ListOrder{
+        Random,
+        Normal,
+        Inverse;
+    }
+    private enum SelectionID {
+        All("*"),
+        SubjectLong("SubjectLong"),
+        SubjectID("SubjectID");
+        final String id;
+        SelectionID(String id){
+            this.id = id;
+        }
+        String getSelectionID(){
+            return id;
+        }
+    }
     private enum TableID{
         CardTypes("CardTypes"),
         Outlines("Outlines"),
@@ -50,6 +69,7 @@ public class DBReader {
     private Connection con;
     private String[] tableNames, dataNames;
     private Statement stmnt;
+    private Random random;
     public DBReader(Connection con){
         this.con = con;
         try {
@@ -57,17 +77,33 @@ public class DBReader {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        random = new Random();
     }
-    private ResultSet getData(TableID table, String selection, String filter){
+    private ResultSet getData(TableID table, SelectionID data, String filter){
         try {
+            return stmnt.executeQuery("SELECT " + data.getSelectionID() + " FROM " + table.getTableID() + " " + filter);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private ResultSet getData(TableID table, SelectionID[] selections, String filter){
+        try {
+            String selection = "";
+            for(int i = 0; i < selections.length; i++){
+                selection += selections[i].getSelectionID();
+                if(i + 1 != selections.length){
+                    selection += " AND ";
+                }
+            }
             return stmnt.executeQuery("SELECT " + selection + " FROM " + table.getTableID() + " " + filter);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
-    public List<Question> getQuestionListBySubject(Subject subject){
-        ResultSet rs = getData(TableID.Questions, "*", "WHERE SubjectID=" + subject.subjectID);
+    public List<Question> getQuestionListBySubject(Subject subject, ListOrder order){
+        ResultSet rs = getData(TableID.Questions, SelectionID.All, "WHERE SubjectID=" + subject.subjectID);
         List<Question> questions = new ArrayList<>();
         try {
             while(rs.next()){
@@ -76,6 +112,22 @@ public class DBReader {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        switch(order){
+            case Random:
+                Collections.shuffle(questions);
+                break;
+            case Inverse:
+                Collections.reverse(questions);
+        }
         return questions;
+    }
+
+    public String getSubjectString(Subject subject){
+        try {
+            return getData(TableID.Subjets, SelectionID.SubjectLong, "WHERE " + SelectionID.SubjectID.getSelectionID() + "=" + subject.getSubjectID()).getString(SelectionID.SubjectLong.getSelectionID());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
