2adc03ee6d7f1559a305446904db3ce858e3b6ab
MrFlyingbananas <mrflyingbananas@yahoo.com>
2016-07-19 13:58:43 -0500

    Added to store test data

---
+++ src/QuestionPane/Test.java
@@ -0,0 +1,50 @@
+package QuestionPane;
+
+import QuestionPane.DBReader.Subject;
+
+import java.util.List;
+/**
+ * Created by mrfly on 7/19/2016.
+ */
+public class Test {
+
+
+    private enum TestDataID{
+        TestID("TestID"),
+        TestSetID("TestSetID"),
+        TestName("TestName"),
+        Subtitle("Subtitle"),
+        SubjectID("SubjectID");
+
+        final String id;
+        TestDataID(String id){
+            this.id = id;
+        }
+        String getTestDataID(){
+            return id;
+        }
+    }
+    private String subtitle;
+    private List<Question> questions;
+    private int testID;
+    private Subject subject;
+    private int testNumber;
+    public Test(Subject subject, int testID, int testNumber, String subtitle, List<Question> questions){
+        this.subject = subject;
+        this.testID = testID;
+        this.subtitle = subtitle;
+        this.questions = questions;
+        this.testNumber = testNumber;
+    }
+    public List<Question> getQuestions(){
+        return questions;
+    }
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+}
