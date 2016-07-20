a0d53f677d084d681d6753d411fc8418c54efa67
MrFlyingbananas <mrflyingbananas@yahoo.com>
2016-07-20 15:16:35 -0500

    Cleaned up code and checked what modules were installed Used for loop for everything

--- src/QuestionPane/TopMenu.java
+++ src/QuestionPane/TopMenu.java
@@ -1,5 +1,8 @@
 package QuestionPane;
 
+import QuestionPane.DBReader.Subject;
+import org.sqlite.core.DB;
+
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -43,144 +46,42 @@ public class TopMenu {
             }
         });
         baseMenu.add(item);
-        if(DBReader.focusQuizzesAdded) {
-            //TODO: A WAY TO SHOW TOP BAR STUFF ONLY IF INSTALLED
+        //TODO: A WAY TO SHOW TOP BAR STUFF ONLY IF INSTALLED
+        Subject[] focusQuizSubjects = DBReader.getFocusQuizSubjects();
+        if(focusQuizSubjects.length != 0){
+            JMenu subMenu = new JMenu("By Subject");
             baseMenu = new JMenu("Focus Quizzes");
             topBar.add(baseMenu);
-            JMenu subMenu = new JMenu("By Subject");
             baseMenu.add(subMenu);
-            item = new JMenuItem(DBReader.getSubjectString(DBReader.Subject.Art));
-            item.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    main.changeQuestionSet(DBReader.getQuestionListBySubject(DBReader.Subject.Art, DBReader.ListOrder.Random));
-                }
-            });
+            for (Subject sub : focusQuizSubjects) {
+                item = new JMenuItem(DBReader.getSubjectString(sub));
+                item.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        main.changeQuestionSet(DBReader.getQuestionListBySubject(sub, DBReader.ListOrder.Random));
+                    }
+                });
+            }
             subMenu.add(item);
-            item = new JMenuItem(DBReader.getSubjectString(DBReader.Subject.Music));
-            item.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    main.changeQuestionSet(DBReader.getQuestionListBySubject(DBReader.Subject.Music, DBReader.ListOrder.Random));
-                }
-            });
-            subMenu.add(item);
-            item = new JMenuItem(DBReader.getSubjectString(DBReader.Subject.LangLit));
-            item.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    main.changeQuestionSet(DBReader.getQuestionListBySubject(DBReader.Subject.LangLit, DBReader.ListOrder.Random));
-                }
-            });
-            subMenu.add(item);
-            item = new JMenuItem(DBReader.getSubjectString(DBReader.Subject.Science));
-            item.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    main.changeQuestionSet(DBReader.getQuestionListBySubject(DBReader.Subject.Science, DBReader.ListOrder.Random));
-                }
-            });
-            subMenu.add(item);
-            item = new JMenuItem(DBReader.getSubjectString(DBReader.Subject.SocialSci));
-            item.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    main.changeQuestionSet(DBReader.getQuestionListBySubject(DBReader.Subject.SocialSci, DBReader.ListOrder.Random));
-                }
-            });
-            subMenu.add(item);
-            item = new JMenuItem(DBReader.getSubjectString(DBReader.Subject.Econ));
-            item.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    main.changeQuestionSet(DBReader.getQuestionListBySubject(DBReader.Subject.Econ, DBReader.ListOrder.Random));
-                }
-            });
-            subMenu.add(item);
-            subMenu = new JMenu("By Test");
+            subMenu = new JMenu("By Quiz");
             baseMenu.add(subMenu);
-
-            JMenu testsMenu = new JMenu(DBReader.getSubjectString(DBReader.Subject.Art) + " Tests");
-            List<Test> tests = DBReader.getTestListBySubject(DBReader.Subject.Art, DBReader.ListOrder.Random);
-            for (Test t : tests) {
-                item = new JMenuItem(t.getSubtitle());
-                item.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        main.changeQuestionSet(t.getQuestions());
-                    }
-                });
-                testsMenu.add(item);
+            for (Subject sub : focusQuizSubjects) {
+                JMenu testsMenu = new JMenu(DBReader.getSubjectString(sub) + " Quizzes");
+                List<Test> tests = DBReader.getTestListBySubject(sub, DBReader.ListOrder.Random);
+                for (Test t : tests) {
+                    item = new JMenuItem(t.getSubtitle());
+                    item.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            main.changeQuestionSet(t.getQuestions());
+                        }
+                    });
+                    testsMenu.add(item);
+                }
+                subMenu.add(testsMenu);
             }
-            subMenu.add(testsMenu);
-
-            testsMenu = new JMenu(DBReader.getSubjectString(DBReader.Subject.Music) + " Tests");
-            tests = DBReader.getTestListBySubject(DBReader.Subject.Music, DBReader.ListOrder.Random);
-            for (Test t : tests) {
-                item = new JMenuItem(t.getSubtitle());
-                item.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        main.changeQuestionSet(t.getQuestions());
-                    }
-                });
-                testsMenu.add(item);
-            }
-            subMenu.add(testsMenu);
-
-            testsMenu = new JMenu(DBReader.getSubjectString(DBReader.Subject.LangLit) + " Tests");
-            tests = DBReader.getTestListBySubject(DBReader.Subject.LangLit, DBReader.ListOrder.Random);
-            for (Test t : tests) {
-                item = new JMenuItem(t.getSubtitle());
-                item.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        main.changeQuestionSet(t.getQuestions());
-                    }
-                });
-                testsMenu.add(item);
-            }
-            subMenu.add(testsMenu);
-            testsMenu = new JMenu(DBReader.getSubjectString(DBReader.Subject.Science) + " Tests");
-            tests = DBReader.getTestListBySubject(DBReader.Subject.Science, DBReader.ListOrder.Random);
-            for (Test t : tests) {
-                item = new JMenuItem(t.getSubtitle());
-                item.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        main.changeQuestionSet(t.getQuestions());
-                    }
-                });
-                testsMenu.add(item);
-            }
-            subMenu.add(testsMenu);
-            testsMenu = new JMenu(DBReader.getSubjectString(DBReader.Subject.SocialSci) + " Tests");
-            tests = DBReader.getTestListBySubject(DBReader.Subject.SocialSci, DBReader.ListOrder.Random);
-            for (Test t : tests) {
-                item = new JMenuItem(t.getSubtitle());
-                item.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        main.changeQuestionSet(t.getQuestions());
-                    }
-                });
-                testsMenu.add(item);
-            }
-            subMenu.add(testsMenu);
-            testsMenu = new JMenu(DBReader.getSubjectString(DBReader.Subject.Econ) + " Tests");
-            tests = DBReader.getTestListBySubject(DBReader.Subject.Econ, DBReader.ListOrder.Random);
-            for (Test t : tests) {
-                item = new JMenuItem(t.getSubtitle());
-                item.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        main.changeQuestionSet(t.getQuestions());
-                    }
-                });
-                testsMenu.add(item);
-            }
-            subMenu.add(testsMenu);
         }
+
         frame.setJMenuBar(topBar);
     }
     public static int getHeight(){
