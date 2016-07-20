53f8c7d0ba6e02cbfec2d157b05160d0e1b95ae1
MrFlyingbananas <mrflyingbananas@yahoo.com>
2016-07-20 15:15:34 -0500

    Added a check for installed bins (only works for focus quizzes this patch)

--- src/QuestionPane/DBReader.java
+++ src/QuestionPane/DBReader.java
@@ -24,13 +24,16 @@ public class DBReader {
         TestSetID("TestSetID"),
         TestID("TestID"),
         TestName("TestName"),
-        Subtitle("Subtitle");
+        Subtitle("Subtitle"),
+        ResourceTypeID("ResourceTypeID");
+
         final String id;
         SelectionID(String id){
             this.id = id;
         }
-        String getSelectionID(){
-            return id;
+        @Override
+        public String toString() {
+            return this.id;
         }
     }
     private enum TableID{
@@ -48,8 +51,9 @@ public class DBReader {
         TableID(String id){
             this.id = id;
         }
-        String getTableID(){
-            return id;
+        @Override
+        public String toString() {
+            return this.id;
         }
     }
     public enum Subject{
@@ -64,14 +68,28 @@ public class DBReader {
         Subject(int subjectID){
             this.subjectID = subjectID;
         }
-
+        public String toString() {
+            return Integer.toString(subjectID);
+        }
         int getSubjectID() {
             return subjectID;
         }
     }
+    private enum ResourceTypeID{
+        FocusQuiz(368),
+        ComprehensiveExam(371);
+
+        final int id;
+        ResourceTypeID(int id){
+            this.id = id;
+        }
+        @Override
+        public String toString() {
+            return Integer.toString(id);
+        }
+    }
     private Connection con;
-    private String[] tableNames, dataNames;
-    public static boolean focusQuizzesAdded = false;
+    private static Subject[] focusQuizSubjects, levelExamSubjects, sectionExamSubjects, comprehensiveExamSubjects, flashcardSubjects;
     private static Statement stmnt;
     public static void setConnection(Connection con){
         try {
@@ -79,11 +97,32 @@ public class DBReader {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        try {
+            ResultSet rs = getData(SelectionID.All, TableID.TestSets, "WHERE " + SelectionID.ResourceTypeID + "=" + ResourceTypeID.FocusQuiz);
+            List<Integer> list = new ArrayList<>();
+            while(rs.next()){
+                list.add(rs.getInt(SelectionID.SubjectID.toString()));
+            }
+            focusQuizSubjects = new Subject[list.size() - 1];
+            int count = 0;
+            for(int j = 0; j < Subject.values().length; j++){
+                Subject sub = Subject.values()[j];
+                for(int i = 0; i < list.size(); i++){
+                    if(sub.getSubjectID() == list.get(i)){
+                        list.remove(i);
+                        focusQuizSubjects[count++] = sub;
+                        break;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     private static ResultSet getData(SelectionID selection, TableID table, String filter){
         try {
-            return stmnt.executeQuery("SELECT " + selection.getSelectionID() + " FROM " + table.getTableID() + " " + filter);
+            System.out.println("SELECT " + selection+ " FROM " + table + " " + filter);
+            return stmnt.executeQuery("SELECT " + selection+ " FROM " + table + " " + filter);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -93,12 +132,12 @@ public class DBReader {
         try {
             String selection = "";
             for(int i = 0; i < selections.length; i++){
-                selection += selections[i].getSelectionID();
+                selection += selections[i];
                 if(i + 1 != selections.length){
                     selection += " AND ";
                 }
             }
-            return stmnt.executeQuery("SELECT " + selection + " FROM " + table.getTableID() + " " + filter);
+            return stmnt.executeQuery("SELECT " + selection + " FROM " + table + " " + filter);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -109,12 +148,12 @@ public class DBReader {
         try {
             String table = "";
             for(int i = 0; i < tables.length; i++){
-                table += tables[i].getTableID();
+                table += tables[i];
                 if(i + 1 != tables.length){
                     table += " AND ";
                 }
             }
-            return stmnt.executeQuery("SELECT " + selection.getSelectionID() + " FROM " + table + " " + filter);
+            return stmnt.executeQuery("SELECT " + selection + " FROM " + table + " " + filter);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -143,7 +182,8 @@ public class DBReader {
 
     public static String getSubjectString(Subject subject){
         try {
-            return getData(SelectionID.SubjectLong, TableID.Subjets, "WHERE " + SelectionID.SubjectID.getSelectionID() + "=" + subject.getSubjectID()).getString(SelectionID.SubjectLong.getSelectionID());
+            System.out.println(subject);
+            return getData(SelectionID.SubjectLong, TableID.Subjets, "WHERE " + SelectionID.SubjectID + "=" + subject.getSubjectID()).getString(SelectionID.SubjectLong.toString());
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -154,18 +194,18 @@ public class DBReader {
         List<Test> tests = new ArrayList<Test>();
         int testSetID = 0;
         try {
-            testSetID = getData(SelectionID.TestSetID, TableID.TestSets, "WHERE " + SelectionID.SubjectID.getSelectionID() + "=" + subject.getSubjectID()).getInt(SelectionID.TestSetID.getSelectionID());
-            ResultSet testSets = getData(SelectionID.All, TableID.Tests, "WHERE " + SelectionID.TestSetID.getSelectionID() + "=" + testSetID);
+            testSetID = getData(SelectionID.TestSetID, TableID.TestSets, "WHERE " + SelectionID.SubjectID + "=" + subject.getSubjectID()).getInt(SelectionID.TestSetID.toString());
+            ResultSet testSets = getData(SelectionID.All, TableID.Tests, "WHERE " + SelectionID.TestSetID + "=" + testSetID);
             List<String> subtitles = new ArrayList<String>();
             List<Integer> testIDs = new ArrayList<Integer>(), testNames = new ArrayList<Integer>();
             List<List<Question>> questionSets = new ArrayList<>();
             while (testSets.next()) {
-                testIDs.add(testSets.getInt(SelectionID.TestID.getSelectionID()));
-                testNames.add(testSets.getInt(SelectionID.TestName.getSelectionID()));
-                subtitles.add(testSets.getString(SelectionID.Subtitle.getSelectionID()));
+                testIDs.add(testSets.getInt(SelectionID.TestID.toString()));
+                testNames.add(testSets.getInt(SelectionID.TestName.toString()));
+                subtitles.add(testSets.getString(SelectionID.Subtitle.toString()));
             }
             for(int testID : testIDs){
-                ResultSet resultSet = getData(SelectionID.All, TableID.Questions, "WHERE " + SelectionID.TestID.getSelectionID() + "=" + testID);
+                ResultSet resultSet = getData(SelectionID.All, TableID.Questions, "WHERE " + SelectionID.TestID + "=" + testID);
                 List<Question> questions = new ArrayList<Question>();
                 while(resultSet.next()){
                     questions.add(new Question(resultSet));
@@ -188,5 +228,8 @@ public class DBReader {
             e.printStackTrace();
         }
         return tests;
+    }
+    public static Subject[] getFocusQuizSubjects(){
+        return focusQuizSubjects;
     }
 }
